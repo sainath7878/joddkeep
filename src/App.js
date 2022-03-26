05cf297f7cc58e02e92afc2e7fb8b63ec93c5d1a
Sainath <svmsainath@ymail.com>
2022-03-26 23:46:57 +0530

    Added Landing & Auth Pages

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
 import './App.css';
+import Mockman from "mockman-js";
+import { Routes, Route } from "react-router-dom";
+import { LandingPage, AuthorizationPage } from 'pages/index'
+import { Header, Footer } from "components/index"
 
 function App() {
   return (
     <div className="App">
-      
+      <Header />
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/signin" element={<AuthorizationPage />} />
+        <Route path="/signup" element={<AuthorizationPage />} />
+        <Route path="/mockman" element={<Mockman />} />
+      </Routes>
+      <Footer />
     </div>
   );
 }
